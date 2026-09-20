b00869e5408fb62807f2ce493f5aff2cf8f7f611
LineChr <Line_Anette96@hotmail.com>
2026-09-20 14:39:06 +0200

    added routing and header

---
+++ src/pages/PlayerSelect.tsx
@@ -0,0 +1,9 @@
+
+
+export default function PlayerSelect() {
+        return(
+            <main>
+                <h1>Spiller 1</h1>
+            </main>
+        )
+}
---
+++ src/pages/Rules.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Rules() {
+        return(
+            <main>
+                <h1>Rules</h1>
+            </main>
+        )
+}
